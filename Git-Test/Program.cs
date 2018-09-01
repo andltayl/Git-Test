e36beee13d1c378da796cf4d37319068cf8a5b0e
Anderson Taylor <andltayl@ut.utm.edu>
2018-09-01 18:29:17 -0500

    Now it's a number guessing game.

--- Git-Test/Program.cs
+++ Git-Test/Program.cs
@@ -10,7 +10,45 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Thinking of a number between 1-100...");
+
+            Random r = new Random();
+            int n = r.Next(0,100);
+
+            bool lp = true;
+            int tries = 1;
+
+            while (lp)
+            {
+
+                Console.Write("Guess? ");
+
+                int g = Convert.ToInt32(Console.ReadLine());
+
+                if (g == n)
+                {
+                    Console.WriteLine("\nCongrats! You got it!");
+                    lp = false;
+                }
+                else
+                {
+                    if (g > n)
+                    {
+                        Console.WriteLine("\nToo High! Guess Again!");
+                        tries++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nToo Low! Guess Again!");
+                        tries++;
+                    }
+                }
+            }
+            Console.Write("\nIt took you ");
+            Console.Write(tries);
+            Console.Write(" tries to get it right!");
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
